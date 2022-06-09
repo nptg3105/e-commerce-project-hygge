28a25d9aff65db00f94c1859756c7b97e8e04d28
Leon Nguyen <nptgiang3105@gmail.com>
2022-06-09 08:24:13 +0700

    fix product page

---
+++ .history/src/components/Products/components/ProductList_20220609080748.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import '../Product.scss'
+import Product from './Product'
+
+const ProductList = ({ productList }) => (
+	<div>
+		{/* <ProductFilter filters={filters} oncChange={handleFiltersChange} /> */}
+		<div className="product_list">
+			{productList.map(product => (
+				<div key={product.id}>
+					<Product product={product} />
+				</div>
+			))}
+		</div>
+	</div>
+)
+ProductList.propTypes = {
+	productList: PropTypes.array
+}
+
+ProductList.defaultProps = {
+	productList: []
+}
+
+export default ProductList
